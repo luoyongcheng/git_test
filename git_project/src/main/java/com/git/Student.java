dff471eadfcd882e947bdf48ea0b951c855894dd
luoyongcheng <luocysy@163.com>
2020-09-27 16:17:03 +0800

    添加一个Student类

---
+++ git_project/src/main/java/com/git/Student.java
@@ -0,0 +1,37 @@
+package com.git;
+
+/**
+ * @description:
+ * @date: 2020/9/27 16:15
+ */
+public class Student {
+    private String name;
+    private Integer age;
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
