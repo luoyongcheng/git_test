87ded442fed457a6da1ecd44c42af0c438d217ec
luoyongcheng <luocysy@163.com>
2020-09-27 15:51:57 +0800

    新建一个Teacher类，提交到本地仓库

---
+++ git_project/src/main/java/com/git/Teacher.java
@@ -0,0 +1,37 @@
+package com.git;
+
+/**
+ * @description:
+ * @date: 2020/9/27 15:41
+ */
+public class Teacher {
+    private String name;
+    private Integer age;
+
+    public Teacher() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
